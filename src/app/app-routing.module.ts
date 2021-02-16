fd5cab75fcdf3ec8650f4fab7cd9580710fd3dfc
Andres Rosales <andresrosalespinzon@gmail.com>
2021-02-16 22:15:56 +0100

    typo importing 404 component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { Routes } from '@angular/router';
 import { BlogComponent } from './Components/blog/blog.component';
 import { FormularioComponent } from './Components/formulario/formulario.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'blog', component: BlogComponent },
   { path: 'new', component: FormularioComponent },
   { path: '', pathMatch: 'full', redirectTo: '/blog' },
-  // { path: "404", component: NotFoundComponent },
-
-  // { path: "**", redirectTo: "/404" },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404' },
 ];
 
 @NgModule({
